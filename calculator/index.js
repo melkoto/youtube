289d453cdddd5ae50a50146c88f0dda75d07b03b
Movsar Makhmutov <m.movsar@gmail.com>
2023-03-10 10:20:41 +0300

    update

--- calculator/index.js
+++ calculator/index.js
@@ -1,13 +1,79 @@
+// const container = document.createElement('div')
+// container.className = "container"
+// document.body.append(container)
+//
+// const h1 = document.createElement('h1')
+// h1.innerText = "Calculator"
+// container.append(h1)
+//
+// const error = document.createElement('div')
+// error.className = 'error'
+// error.innerText = 'Input number'
+// container.append(error)
+//
+// const input = document.createElement('input')
+// input.type = 'text'
+// input.className = 'input'
+// container.append(input)
+//
+// const buttons = document.createElement('div')
+// buttons.className = 'buttons'
+// container.append(buttons)
+//
+// const mathOperations = document.createElement('div')
+// mathOperations.className = 'math-operations'
+// buttons.append(mathOperations)
+//
+// const addition = document.createElement('button')
+// addition.classList.add('math-operation', 'addition')
+// addition.innerText = '+'
+// mathOperations.append(addition)
+//
+// const subtraction = document.createElement('button')
+// subtraction.classList.add('math-operation', 'subtraction')
+// subtraction.innerText = '-'
+// mathOperations.append(subtraction)
+//
+// const division = document.createElement('button')
+// division.classList.add('math-operation', 'division')
+// division.innerText = '/'
+// mathOperations.append(division)
+//
+//
+// const multiplication = document.createElement('button')
+// multiplication.classList.add('math-operation', 'multiplication')
+// multiplication.innerText = 'x'
+// mathOperations.append(multiplication)
+//
+// const result = document.createElement('button')
+// result.className = 'result'
+// result.innerText = '='
+// buttons.append(result)
+//
+// const history = document.createElement('div')
+// history.className = 'history'
+// container.append(history)
+//
+// const h2 = document.createElement('h2')
+// h2.innerText = 'History'
+// history.append(h2)
+//
+// const calculation = document.createElement('div')
+// calculation.className = 'calculation'
+// calculation.innerText = '1 + 2 = 3'
+// history.append(calculation)
+
+
+const input = document.querySelector('.input')
+input.addEventListener('keydown', getInput)
+
 const error = document.querySelector('.error')
 const history = document.querySelector('.history')
 
 const allMathOperations = document.querySelectorAll('.math-operation')
-allMathOperations.forEach(mathOperation => {
-    mathOperation.addEventListener('click', getMathOperation)
+allMathOperations.forEach(button => {
+    button.addEventListener('click', getMathOperation)
 })
-
-const input = document.querySelector('.input')
-input.addEventListener('keydown', getInput)
 
 const result = document.querySelector('.result')
 result.addEventListener('click', getResult)
@@ -23,6 +89,7 @@ function getInput(event) {
             return
         }
     }
+
     return input.value
 }
 
@@ -38,38 +105,41 @@ function getMathOperation(event) {
     currBtn.classList.add('current-operation')
 
     let first = getInput()
-    if (!first) return
+
+    if (!first) return;
     firstNumber = Number(first)
 
-    switch (event.target.innerText) {
+    switch (currBtn.innerText) {
         case '+':
             first += ' +'
             break
         case '-':
             first += ' -'
             break
+        case 'x':
+            first += ' x'
+            break
         case '/':
             first += ' /'
             break
-        case 'x':
-            first += ' x'
     }
 
-    const div = document.createElement('div')
-    div.className = 'calculation'
-    div.innerText = first
-    history.append(div)
+    const calculation = document.createElement('div')
+    calculation.className = 'calculation'
+    calculation.innerText = first
+    history.append(calculation)
 
-    input.value = ""
+    input.value = ''
     input.focus()
 }
 
-function getResult() {
+function getResult(event) {
+    event.preventDefault()
     if (!firstNumber) return
 
     const mathOperations = document.querySelectorAll('.math-operation')
-    mathOperations.forEach(op => {
-        op.classList.remove('current-operation')
+    mathOperations.forEach(button => {
+        button.classList.remove('current-operation')
     })
 
     const calculations = document.querySelectorAll('.calculation')
@@ -105,16 +175,10 @@ function getResult() {
     }
 
     lastCalculation.innerText = calculation
-    input.value = ""
-    firstNumber = null
+    input.value =""
     input.focus()
-}
+    firstNumber = null
 
-function showError() {
-    error.style.display = 'block'
-    setTimeout(() => {
-        error.style.display = 'none'
-    }, 1000)
 }
 
 function isNumeric(keyboardEvent) {
@@ -122,10 +186,49 @@ function isNumeric(keyboardEvent) {
     return charCode > 47 && charCode < 58
 }
 
-window.addEventListener("beforeunload", () => {
+function showError() {
+    error.style.display = 'block'
+
+    setTimeout(() => {
+        error.style.display = 'none'
+    }, 1000)
+}
+
+window.addEventListener('beforeunload', () => {
     input.removeEventListener('keydown', getInput)
-    allMathOperations.forEach(operation => {
-        operation.removeEventListener('click', getMathOperation)
+
+    allMathOperations.forEach(button => {
+        button.removeEventListener('click', getMathOperation)
     })
+
     result.removeEventListener('click', getResult)
-});
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
